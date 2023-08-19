302955a50e47ab32ee8c9cdc312f473da572e8a1
Rolf <rolf@scherer.ml>
2023-08-19 14:43:33 +0200

    Main-layout added

--- src/app/layouts/main-layout/main-layout.component.ts
+++ src/app/layouts/main-layout/main-layout.component.ts
@@ -1,10 +1,14 @@
 import {Component} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {SidebarComponent} from "../components/sidebar/sidebar.component";
+import {NavbarComponent} from "../components/navbar/navbar.component";
+import {RouterOutlet} from "@angular/router";
+import {FooterComponent} from "../components/footer/footer.component";
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SidebarComponent, NavbarComponent, RouterOutlet, FooterComponent],
   templateUrl: './main-layout.component.html',
   styleUrls: ['./main-layout.component.scss']
 })
